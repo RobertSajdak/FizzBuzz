290ed5ecb5c0b294b81fdd197f763d785f5e87e7
RobertSajdak <robert.sajdak@onet.eu>
2020-12-09 14:54:21 +0100

    Add program files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public FizzBuzz()
+        {
+
+        }
+
+        public string GetOutput(int number)
+        {
+            if (number % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            while (true)
+            {
+                Console.WriteLine("Podaj proszę swoją liczbę...");
+
+                while (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Podana przez Ciebie wartość NIE JEST LICZBĄ! Spróbuj ponownie.");
+                }
+
+                var fizzBuzz = new FizzBuzz(); //inicjalizacja obiektu klasy FizzBuzz
+                Console.WriteLine(fizzBuzz.GetOutput(number)); //wypisanie wyniku w konsoli na wywołanej metodzie
+            }
+        }
+    }
+}
